25bc57876e2ee522e33b6ec9fbb1db76330688d3
swagsbots <swagbots@gmail.com>
2025-08-28 10:32:49 +0300

    Final fix: Python 3.11 + no pandas

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel
-pip install --only-binary :all: -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# Find Python executable
-PYTHON_CMD=$(which python3 || which python)
-
-if [ -z "$PYTHON_CMD" ]; then
-    echo "Python not found! Available options:"
-    ls /usr/bin/python*
-    exit 1
-fi
-
+# Find Python explicitly
+PYTHON_CMD=$(which python3.11 || which python3 || which python)
 echo "Using Python: $PYTHON_CMD"
-echo "Python version:"
-$PYTHON_CMD --version
-
-# Run the application
 $PYTHON_CMD bitget_trading_bot.py
